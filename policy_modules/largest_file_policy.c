e8a262b4b3e8ca71acfa0f5fafd5b5032131c634
Sascha Thiemann <saschathiemann00@gmail.com>
2024-02-21 15:29:56 +0100

    largest_file_policy.c now follows coding guidlines

--- policy_modules/largest_file_policy.c
+++ policy_modules/largest_file_policy.c
@@ -19,27 +19,27 @@ static struct eviction_policy file_size_policy = {
 
 /**
  * lf_compare - Compares two inodes and returns the largest.
- * 
+ *
  * @first: First node to compare.
  * @second: Second node to compare.
- * 
+ *
  * Return: The node with larger size, NULL if both nodes are NULL.
- */ 
-static struct inode *lf_compare(struct inode *first, struct inode *second) 
+ */
+static struct inode *lf_compare(struct inode *first, struct inode *second)
 {
-	if(!first)
+	if (!first)
 		return second;
-	if(!second)
+	if (!second)
 		return first;
 
-	// Compare size of files
-	if(first->i_size < second->i_size)
+	/* Compare size of files */
+	if (first->i_size < second->i_size)
 		return second;
 	else
 		return first;
 }
 
-static int __init largest_file_policy_init(void) 
+static int __init largest_file_policy_init(void)
 {
 	int errc = register_policy(&file_size_policy);
 
@@ -47,7 +47,7 @@ static int __init largest_file_policy_init(void)
 }
 module_init(largest_file_policy_init);
 
-static void __exit largest_file_policy_exit(void) 
+static void __exit largest_file_policy_exit(void)
 {
 	unregister_policy(&file_size_policy);
 }
